a315e8cbd6c559c7509b454a8e01ba8a66434e2a
Emmanuel Yeboah <emmanuelyeboah@example.com>
2026-01-19 20:57:49 +0100

    Increase header logo size (Option 1: Moderate increase)

--- app/components/SiteChrome.tsx
+++ app/components/SiteChrome.tsx
@@ -111,7 +111,7 @@ export function Header() {
               src="/images/logo/master/fairways-master-logo.png"
               alt="Fairways.Tech"
               className={`transition-all duration-300 hover:scale-105 ${
-                isScrolled ? "h-8 w-8" : "h-10 w-10 sm:h-12 sm:w-12"
+                isScrolled ? "h-10 w-10" : "h-12 w-12 sm:h-14 sm:w-14"
               }`}
             />
             <div
